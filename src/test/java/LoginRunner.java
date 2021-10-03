fb809edbff6af8a5c3a796f92d4fd66203e5326e
Kuba Zhaanbaev <k.zhaanbaev4@gmail.com>
2021-10-03 11:16:33 -0400

    added json plugin option for cucumber jenkins report

--- src/test/java/LoginRunner.java
+++ src/test/java/LoginRunner.java
@@ -5,7 +5,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"pretty", "html:target/loginRunner.html",
-        "rerun:target/failed-scenarios/login.txt"},
+        "rerun:target/failed-scenarios/login.txt",
+        "json:target/cucumberLoginRunner.json"},
         features = "src/main/resources/features/login.feature",
         glue = {"steps"},
         stepNotifications = true
